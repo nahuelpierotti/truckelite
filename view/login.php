55a59a5772c5a775d930b86c2b038712d024e47d
Nahuel Pierotti <npie@unlam.edu.ar>
2020-11-10 19:36:11 -0300

    Corrige borrado del login

---
+++ view/login.php
@@ -0,0 +1,21 @@
+{{> header}}
+<section>
+    <article class="w3-container w3-content w3-white">
+    <div class="w3-center w3-container w3-content">
+        <img src="view/img/logo.png" style="width: 20em;">
+    </div>
+    <form action="login/procesarLogin" class="w3-container w3-content w3-padding w3-card w3-white  w3-border w3-round w3-mobile" style="width:40%; margin-top: 1em" method="POST">
+            <div class="w3-center w3-padding-16">
+                <img src="view/img/user3.png" style="width: 20%;">
+            </div>
+            <label for="" class="lbl-nombre">Usuario</label>
+            <input type="text" class="w3-input form-input-login" name="nombre">
+            <label for="">Clave</label>
+            <input type="password" class="w3-input form-input-login" name="clave">
+        <button class="w3-btn w3-blue w3-block w3-round w3-margin-bottom" style="margin-top: 2em">Log In</button>
+        <p>¿Te olvidaste la clave? <a href="#cambiarClave" class="w3-text-blue link-estilo w3-hover-text-blue-grey">Cambiar clave</a><br>
+        ¿No tenes cuenta?Registrate aca <a href="registrar" class="w3-text-blue link-estilo w3-hover-text-blue-grey">Registrarse</a></p>
+    </form>
+    </article>
+</section>
+{{> footer}}
